a19aebd875278f7949068aa73e44bf1775ee0a22
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-12-05 23:37:42 -0500

    (Day 6) both puzzles completed, some shameless borrowing from Reddit for part 2

--- aoc/util.py
+++ aoc/util.py
@@ -8,3 +8,8 @@ def find_file(filename: str) -> Path:
 def get_input_array(filename: str) -> list[str]:
     with open(find_file(filename)) as file:
         return [line.rstrip() for line in file]
+
+
+def get_split_input(filename: str, delimiter: str) -> list[str]:
+    with open(find_file(filename)) as file:
+        return file.read().split(delimiter)
