4db57c5da90a65895cdb38e992e284d9c284a3b1
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-12-09 00:37:17 -0500

    (Day 9) second puzzle

--- aoc/puzzles/day9.py
+++ aoc/puzzles/day9.py
@@ -6,8 +6,12 @@ from aoc.util import string_to_list
 def get_next(seq: list[int], rev: bool = False) -> int:
     p = 0
     while seq:
-        p += seq[-1]
-        seq = [b - a for a, b in itertools.pairwise(seq)]
+        if rev:
+            p += seq[0]
+            seq = [a - b for a, b in itertools.pairwise(seq)]
+        else:
+            p += seq[-1]
+            seq = [b - a for a, b in itertools.pairwise(seq)]
 
     return p
 
@@ -15,3 +19,8 @@ def get_next(seq: list[int], rev: bool = False) -> int:
 def puzzle1(data: list[str]) -> int:
     seq = [string_to_list(line) for line in data]
     return sum(get_next(s) for s in seq)
+
+
+def puzzle2(data: list[str]) -> int:
+    seq = [string_to_list(line) for line in data]
+    return sum(get_next(s, True) for s in seq)
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -1,4 +1,4 @@
-from aoc.puzzles.day9 import puzzle1
+from aoc.puzzles.day9 import puzzle1, puzzle2
 from aoc.util import get_input_array
 
 
@@ -11,3 +11,14 @@ def test_puzzle1_sample_data():
 def test_puzzle1():
     data = get_input_array("day9.txt")
     print("DAY 9, PUZZLE 1 ANSWER:", puzzle1(data))
+
+
+def test_puzzle2_sample_data():
+    data = get_input_array("day9-sample.txt")
+    result = puzzle2(data)
+    assert result == 2
+
+
+def test_puzzle2():
+    data = get_input_array("day9.txt")
+    print("DAY 9, PUZZLE 2 ANSWER:", puzzle2(data))
