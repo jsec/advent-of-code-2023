65b357f6c5f5b026188d7a4b905336415fa753f4
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-12-17 15:05:28 -0500

    (Day 14) part 2

--- aoc/puzzles/day14.py
+++ aoc/puzzles/day14.py
@@ -1,29 +1,55 @@
 import numpy as np
 
 
+def p1(data: str):
+    beams = parse_input(data)
+    return sum([load(tilt(beam)) for beam in beams])
+
+
+def p2(data: str, cycles: int):
+    beams = cycle(parse_input(data), cycles)
+    return sum([load(beam) for beam in beams])
+
+
 def parse_input(data: str):
     beams = [line.strip() for line in data.splitlines()]
     return roll(beams)
 
 
-def roll(beams: list[str]):
-    arr = np.rot90([list(beam) for beam in beams])
+def cycle(beams: list[str], target: int):
+    count = 0
+    cache = {}
+    cache_hit = False
+
+    while count < target:
+        for i in [0, 3, 3, 3]:  # counter-clockwise rotations for N, W, S, E
+            beams = [tilt(beam) for beam in roll(beams, i)]
+
+        # get back to north-orientation
+        beams = roll(beams, 3)
+
+        count += 1
+        key = "".join(beams)
+
+        if not cache_hit and (cache_hit := key in cache):
+            cycle_length = count - cache[key]
+            count += cycle_length * ((target - count) // cycle_length)
+        else:
+            cache[key] = count
+
+    return beams
+
+
+def roll(beams: list[str], n: int = 1) -> list[str]:
+    arr = np.rot90([list(beam) for beam in beams], n)
     return ["".join(beam) for beam in arr]
 
 
-def p1(data: str):
-    beams = parse_input(data)
-    return sum([get_load(tilt(beam)) for beam in beams])
-
-
 def tilt(beam: str):
-    while beam.find(".O") >= 0:
-        beam = beam.replace(".O", "O.")
-
-    return beam
+    return "#".join("".join(sorted(p, reverse=True)) for p in beam.split("#"))
 
 
-def get_load(beam: str) -> int:
+def load(beam: str) -> int:
     result = 0
     for idx, char in enumerate(beam):
         if char == "O":
--- tests/test_14.py
+++ tests/test_14.py
@@ -1,4 +1,4 @@
-from aoc.puzzles.day14 import p1
+from aoc.puzzles.day14 import p1, p2
 from aoc.util import get_raw_input
 
 
@@ -10,3 +10,13 @@ def test_puzzle1_sample():
 
 def test_puzzle1():
     print("DAY 14, PUZZLE 1 ANSWER:", p1(get_raw_input("14.txt")))
+
+
+def test_puzzle2_sample():
+    data = get_raw_input("14-test.txt")
+    result = p2(data, 10**9)
+    assert result == 64
+
+
+def test_puzzle2():
+    print("DAY 14, PUZZLE 2 ANSWER:", p2(get_raw_input("14.txt"), 10**9))
