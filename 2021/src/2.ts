70f0e80331089a8bb97a4d5049049060bd2ad36f
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2023-12-30 00:50:29 -0500

    (2021 - Day 2) both parts

---
+++ 2021/src/2.ts
@@ -0,0 +1,46 @@
+import { getInputLines } from './util/input'
+
+const input = getInputLines().map(l => l.split(' '))
+
+const solve = (input, p2 = false) => {
+  let horizontal = 0
+  let depth = 0
+  let aim = 0
+
+  for (const step of input) {
+    const direction = step[0]
+    const length = parseInt(step[1]!)
+
+    switch (direction) {
+      case 'forward':
+        horizontal += length
+        if (p2) {
+          depth += aim * length
+        }
+        break
+      case 'up':
+        if (p2) {
+          aim -= length
+        }
+        else {
+          depth -= length
+        }
+        break
+      case 'down':
+        if (p2) {
+          aim += length
+        }
+        else {
+          depth += length
+        }
+        break
+      default:
+        break
+    }
+  }
+
+  return horizontal * depth
+}
+
+console.log('P1:', solve(input))
+console.log('P2:', solve(input, true))
