06e51889d42d0dc97c5df1cf5c6632f7bbf033be
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-01-01 14:59:10 -0500

    (2021 - Day 3) part 2

--- 2021/src/3.ts
+++ 2021/src/3.ts
@@ -1,21 +1,49 @@
-import { zip } from './util/array'
 import { getInputLines } from './util/input'
 
-const getPower = (readings: string[][]): number => {
+const getPower = (readings: string[]) => {
   let gamma = ''
   let epsilon = ''
 
-  for (const reading of readings) {
-    const zeroes = reading.filter(x => !x).length
-    const ones = reading.filter(x => x).length
+  for (let i = 0; i < readings[0].length; i++) {
+    const zeroes = readings.filter(r => r[i] === '0').length
+    const ones = readings.filter(r => r[i] === '1').length
 
     gamma += zeroes > ones ? '0' : '1'
     epsilon += zeroes > ones ? '1' : '0'
   }
 
-  return parseInt(epsilon, 2) * parseInt(gamma, 2)
+  return {
+    epsilon,
+    gamma,
+  }
 }
 
-const readings = zip(getInputLines()).map(l => l.map(i => parseInt(i)))
+const getRating = (readings: string[], leastCommon = false) => {
+  for (let i = 0; i < readings[0].length; i++) {
+    let bit = ''
+    const zeroes = readings.filter(r => r[i] === '0').length
+    const ones = readings.filter(r => r[i] === '1').length
 
-console.log('P1:', getPower(readings))
+    if (zeroes > ones) {
+      bit = leastCommon ? '1' : '0'
+    } else if (ones > zeroes) {
+      bit = leastCommon ? '0' : '1'
+    } else {
+      bit = leastCommon ? '0' : '1'
+    }
+
+    readings = readings.filter(r => r[i] === bit)
+    if (readings.length === 1) {
+      return parseInt(readings[0], 2)
+    }
+  }
+}
+
+const readings = getInputLines()
+const power = getPower(readings)
+const p1 = parseInt(power.epsilon, 2) * parseInt(power.gamma, 2)
+console.log('P1:', p1)
+
+const oxygen = getRating(readings)
+const co2 = getRating(readings, true)
+console.log('P2:', oxygen * co2)
