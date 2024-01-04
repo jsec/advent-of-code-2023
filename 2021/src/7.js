89f0c552c1432c6dc8390684f664fe04ad70e24c
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-01-04 15:51:34 -0500

    (2021 - Day 7) part 2

--- 2021/src/7.js
+++ 2021/src/7.js
@@ -1,12 +1,27 @@
 const { getInput } = require('./util/input')
 
+/*
+ * P1 is just a straight difference between the initial position and
+ * the candidate alignment position
+ */
+const p1 = (arr, k) => arr.map(a => Math.abs(a - k)).reduce((a, c) => a + c, 0)
+
+/*
+ * P2 is the nth triangular number where n is the difference between the
+ * initial position and the candidate alignment position
+ */
+const p2 = (arr, k) =>
+  arr.map(a => {
+    const diff = Math.abs(a - k)
+    return (diff * (diff + 1)) / 2
+  }).reduce((a, c) => a + c, 0)
+
+const run = (input, mapFn) => {
+  return Math.min(...Array.from(Array(max).keys()).map(k => mapFn(input, k)))
+}
+
 const input = getInput().trim().split(',').map(i => parseInt(i))
 const max = Math.max(...input)
 
-const p1 = Math.min(
-  ...Array.from(Array(max).keys()).map(k => {
-    return input.map(i => Math.abs(i - k)).reduce((acc, curr) => acc + curr, 0)
-  }),
-)
-
-console.log('P1:', p1)
+console.log('P1:', run(input, p1))
+console.log('P2:', run(input, p2))
