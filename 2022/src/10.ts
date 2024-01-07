a8f1162ebc2807a5d9e073d813a725c250193c92
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-01-06 22:04:01 -0500

    (2022 - Day 10) part 1

---
+++ 2022/src/10.ts
@@ -0,0 +1,22 @@
+import { getInputLines } from './util/input'
+
+const input = getInputLines()
+
+const cycles: number[] = []
+
+for (const line of input) {
+  cycles.push(0)
+
+  if (!line.startsWith('noop')) {
+    cycles.push(parseInt(line.split(' ')[1]))
+  }
+}
+
+const signals = [20, 60, 100, 140, 180, 220]
+const p1 = signals
+  .map(s => cycles
+    .slice(0, s - 1)
+    .reduce((a, c) => a + c, 1) * s)
+  .reduce((a, c) => a + c, 0)
+
+console.log('P1:', p1)
