2156080c71d8d7b29479b1b229a3b1190334fd34
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-01-17 13:13:00 -0500

    (2016 - Day 5) part 2

--- 2016/src/5.ts
+++ 2016/src/5.ts
@@ -4,16 +4,44 @@ import { getInput } from './util/input'
 
 const input = getInput()
 
-let password = ''
-let i = 0
-
-while (password.length !== 8) {
-  const value = input + i
-  const hash = createHash('md5').update(value).digest('hex')
-  if (hash.startsWith('00000')) {
-    password += hash[5]
-  }
-  i += 1
+const getHash = (value: string): string => {
+  return createHash('md5').update(value).digest('hex')
 }
 
-console.log('P1:', password)
+const p1 = (input: string): string => {
+  let password = ''
+  let i = 0
+  while (password.length !== 8) {
+    const value = input + i
+    const hash = getHash(value)
+    if (hash.startsWith('00000')) {
+      password += hash[5]
+    }
+    i += 1
+  }
+
+  return password
+}
+
+const p2 = (input: string): string => {
+  const password = Array(8).fill('')
+  let i = 0
+
+  while (!password.every(p => p !== '')) {
+    const value = input + i
+    const hash = getHash(value)
+    if (hash.startsWith('00000')) {
+      const pos = parseInt(hash[5]!)
+      if (pos < 8 && password[pos] === '') {
+        password[pos] = hash[6]!
+      }
+    }
+
+    i += 1
+  }
+
+  return password.join('')
+}
+
+console.log('P1:', p1(input))
+console.log('P2:', p2(input))
