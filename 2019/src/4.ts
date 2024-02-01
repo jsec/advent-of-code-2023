8f531fde3deafb40b337a92ad2b0df93b5a80d38
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-01-31 21:45:57 -0500

    (2019 - Day 4) part 2

--- 2019/src/4.ts
+++ 2019/src/4.ts
@@ -3,7 +3,7 @@ import { range } from 'remeda'
 import { pairwise } from './util/array'
 import { getInput } from './util/input'
 
-const isValid = (password: string): boolean => {
+const isValid = (password: string, p2 = false): boolean => {
   const pairs = pairwise(password.split('').map(p => parseInt(p)))
 
   let hasRepeating = false
@@ -18,13 +18,24 @@ const isValid = (password: string): boolean => {
     }
   }
 
+  if (p2) {
+    const matches = password.match(/(.)\1+/g) ?? []
+    return matches.some(m => m.length === 2)
+  }
+
   return hasRepeating
 }
 
 const [start, end] = getInput().split('-').map(i => parseInt(i))
+const passwords = range(start!, end!)
 
-const p1 = range(start!, end!)
+const p1 = passwords
   .filter(p => isValid(p.toString()))
   .length
 
+const p2 = passwords
+  .filter(p => isValid(p.toString(), true))
+  .length
+
 console.log('P1:', p1)
+console.log('P2:', p2)
