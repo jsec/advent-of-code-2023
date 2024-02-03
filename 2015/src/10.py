08bde4111149fda250d16b9e0a37c3502a8e9c3f
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-02-03 15:54:37 -0500

    (2015 - Day 10) both parts

--- 2015/src/10.py
+++ 2015/src/10.py
@@ -1,4 +1,33 @@
-def run(data):
+from util.input import get_input
 
 
-data = run("1")
+def calculate(data):
+    count = 1
+    acc = ""
+
+    for idx, char in enumerate(data):
+        if idx == len(data) - 1:
+            acc += str(count) + char
+            return acc
+
+        if char == data[idx + 1]:
+            count += 1
+            continue
+
+        acc += str(count) + char
+        count = 1
+
+    return ""
+
+
+def run(data: str, iterations: int) -> int:
+    for _ in range(iterations):
+        data = calculate(data)
+
+    return len(data)
+
+
+data = get_input().strip()
+
+print("P1:", run(data, 40))
+print("P1:", run(data, 50))
