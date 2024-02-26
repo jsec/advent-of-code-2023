59d0d2fd056a6a0d70f0b753763cd7e11b19fbac
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-02-25 22:46:21 -0500

    (2015 - Day 12) part 2

--- 2015/src/12.py
+++ 2015/src/12.py
@@ -1,9 +1,32 @@
+import json
 import re
 
 from util.input import get_input
 
-json = get_input()
 
-p1 = sum([int(num) for num in re.findall(r"-?\d+", json)])
+def p1(data):
+    return sum([int(num) for num in re.findall(r"-?\d+", data)])
 
-print("P1:", p1)
+
+def no_reds(struct):
+    result = 0
+
+    if isinstance(struct, list):
+        child_sums = [no_reds(child) for child in struct]
+        return sum(child_sums)
+    elif isinstance(struct, dict):
+        for key in struct.keys():
+            if struct[key] == "red":
+                return 0
+
+            result += no_reds(struct[key])
+    elif isinstance(struct, int):
+        result += struct
+
+    return result
+
+
+data = get_input()
+
+print("P1:", p1(data))
+print("P2:", no_reds(json.loads(data)))
