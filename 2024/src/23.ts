f588b70af24e21a715fe0419f5020de9cccc49c6
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-12-24 16:41:22 -0500

    (2024 - Day 23) part 2

--- 2024/src/23.ts
+++ 2024/src/23.ts
@@ -11,16 +11,11 @@ const generateAdjacencyList = () => {
     const a = edge[0]!;
     const b = edge[1]!;
 
-    if (!list[a]) {
-      list[a] = new Set();
-    }
+    list[a] ??= new Set();
+    list[a].add(b);
 
-    if (!list[b]) {
-      list[b] = new Set();
-    }
-
-    list[a].add(b!);
-    list[b].add(a!);
+    list[b] ??= new Set();
+    list[b].add(a);
   }
 
   return list;
@@ -51,6 +46,57 @@ const p1 = (graph: Graph) => {
   ).length;
 };
 
+const p2 = (graph: Graph) => {
+  const cliques = new Set<string>();
+
+  for (const node in graph) {
+    const neighbors = [...graph[node]!];
+
+    for (let x = 0; x < neighbors.length; x++) {
+      for (let y = x + 1; y < neighbors.length; y++) {
+        const n1 = neighbors[x]!;
+        const n2 = neighbors[y]!;
+
+        if (graph[n1]!.has(n2)) {
+          const clique = [node, n1, n2].sort();
+          cliques.add(clique.join(','));
+        }
+      }
+    }
+  }
+
+  function nextCliques(cliques: Set<string>) {
+    const newCliques = new Set<string>();
+
+    for (const clique of cliques) {
+      const arr = clique.split(',');
+
+      for (const other of graph[arr[0]!]!) {
+        if (arr.every(a => graph[other]!.has(a))) {
+          newCliques.add([...arr, other].sort().join(','));
+        }
+      }
+    }
+
+    return newCliques;
+  }
+
+  let currentCliques = cliques;
+
+  while (true) {
+    const next = nextCliques(currentCliques);
+
+    if (next.size === 0) {
+      break;
+    }
+
+    currentCliques = next;
+  }
+
+  return [...currentCliques][0];
+};
+
 const graph = generateAdjacencyList();
 
 console.log('P1:', p1(graph));
+console.log('P2:', p2(graph));
